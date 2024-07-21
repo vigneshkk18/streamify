adf8be0086bd73c1a473e5cbde6fa8e03655e7dc
Vignesh <kkvignesh2000@gmail.com>
2024-07-21 11:35:36 +0530

    fixed chunk issue

--- vite.config.ts
+++ vite.config.ts
@@ -6,30 +6,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id: string) {
-          if (id.includes("@vidstack")) {
-            return "@vidstack";
-          }
-          if (id.includes("socket.io")) {
-            return "socket.io";
-          }
-          if (id.includes("react-file-drop")) {
-            return "react-file-drop";
-          }
-          if (id.includes("@radix-ui")) {
-            return "@radix-ui";
-          }
-          if (id.includes("src")) {
-            return "@core";
-          }
-          return "rest-vendors";
-        },
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
